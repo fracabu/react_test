c76bf006cd288c2bff9b9468b5ac635ee4bb0966
fracabu <100199965+fracabu@users.noreply.github.com>
2023-04-15 18:01:43 +0200

    fix some bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
       </div>
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
-        <button type="button">Sign up</button>
+        <button type="button" class="active pushable">Sign up</button>
       </div>
       <div className="gpt3__navbar-menu">
         {toggleMenu
